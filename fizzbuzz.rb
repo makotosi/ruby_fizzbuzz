111314b2c6e0e7466afbf800426b72046500d1c4
makotosiraisi <makotosiraisi3@gmail.com>
2016-09-10 10:46:09 +0900

    ruby times fizzbuzz 1st

---
+++ fizzbuzz.rb
@@ -0,0 +1,13 @@
+100.times do |i|
+  if i == 0
+    puts i
+  elsif i % 15 == 0
+    puts "FizzBuzz"
+  elsif i % 5 == 0
+    puts "Buzz"
+  elsif i % 3 == 0
+    puts "Fizz"
+  else
+    puts i
+  end
+end
